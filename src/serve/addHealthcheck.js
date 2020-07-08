8e010a36a4b855aed6e7f71ff3545675038a240e
ryanhs <mr.ryansilalahi@gmail.com>
2020-07-08 22:52:52 +0700

    - add cloudnative/health for "/healthcheck/live", "/healthcheck/ready", "/healthcheck/health" - something like `.registerStartupCheck` in healthcheck can be added at hook `.emit('healthcheck:setup', { health, healthcheck })`

--- src/serve/addHealthcheck.js
+++ src/serve/addHealthcheck.js
@@ -1,8 +1,65 @@
+const health = require('@cloudnative/health');
 const ping = require('../defaults/ping');
 
+const StateCode = {
+  OK: 200,
+  DOWN: 503,
+  ERRORED: 500,
+};
+
 module.exports = async ({ koaRouter, graphqlPubSub, hook }) => {
-  // healthchecks ping
-  koaRouter.get('/healthcheck', ping);
+  // healthcheck setup
+  const healthcheck = new health.HealthChecker();
+  await hook.emit('healthcheck:setup', { health, healthcheck });
+
+  async function healthHandler(ctx) {
+    const status = await healthcheck.getStatus();
+    /* eslint-disable */
+    switch (status.status) {
+        case health.State.STARTING:    ctx.status = StateCode.DOWN; break;
+        case health.State.UP:          ctx.status = StateCode.OK;   break;
+        case health.State.DOWN:        ctx.status = StateCode.DOWN; break;
+        case health.State.STOPPING:    ctx.status = StateCode.DOWN; break;
+        case health.State.STOPPED:     ctx.status = StateCode.DOWN; break;
+    }
+    /* eslint-enable */
+    ctx.body = status;
+  }
+
+  async function livenessHandler(ctx) {
+    const status = await healthcheck.getLivenessStatus();
+    /* eslint-disable */
+    switch (status.status) {
+        case health.State.STARTING:    ctx.status = StateCode.OK;   break;
+        case health.State.UP:          ctx.status = StateCode.OK;   break;
+        case health.State.DOWN:        ctx.status = StateCode.DOWN; break;
+        case health.State.STOPPING:    ctx.status = StateCode.DOWN; break;
+        case health.State.STOPPED:     ctx.status = StateCode.DOWN; break;
+    }
+    /* eslint-enable */
+    ctx.body = status;
+  }
+
+  async function readinessHandler(ctx) {
+    const status = await healthcheck.getReadinessStatus();
+    /* eslint-disable */
+    switch (status.status) {
+        case health.State.STARTING:    ctx.status = StateCode.DOWN;   break;
+        case health.State.UP:          ctx.status = StateCode.OK;     break;
+        case health.State.DOWN:        ctx.status = StateCode.DOWN;   break;
+        case health.State.STOPPING:    ctx.status = StateCode.DOWN;   break;
+        case health.State.STOPPED:     ctx.status = StateCode.DOWN;   break;
+    }
+    /* eslint-enable */
+    ctx.body = status;
+  }
+
+  koaRouter.get('/healthcheck/live', livenessHandler);
+  koaRouter.get('/healthcheck/ready', readinessHandler);
+  koaRouter.get('/healthcheck/health', healthHandler);
+  koaRouter.get('/healthcheck', healthHandler);
+
+  // healthchecks by ping
   koaRouter.get('/ping', ping);
   koaRouter.get('/api/ping', ping);
 
@@ -13,7 +70,7 @@ module.exports = async ({ koaRouter, graphqlPubSub, hook }) => {
     graphqlPubSub.publish(channel, payload);
   }, 3000);
 
-  hook.emit('healthcheck:added', { interval });
+  await hook.emit('healthcheck:added', { interval, health, healthcheck });
   hook.on('quit', () => clearInterval(interval));
 
   return { interval };
